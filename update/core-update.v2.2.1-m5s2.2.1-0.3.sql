b3e2e9774b748044c1b85f5eb024166ad4b300bd
ralzakark <alessandro.caputo@gmail.com>
2013-03-25 20:08:44 +0100

    Normalizzazione base dati 	modified:   core.sql 	modified:   update/core-update.v2.2.1-m5s2.2.1-0.3.sql

--- update/core-update.v2.2.1-m5s2.2.1-0.3.sql
+++ update/core-update.v2.2.1-m5s2.2.1-0.3.sql
@@ -1,32 +1,27 @@
 CREATE TABLE "template" (
-        "id"                    integer         PRIMARY KEY,
+        "id"                    INTEGER         PRIMARY KEY,
         "name"                  TEXT,
         "description"           TEXT );
-COMMENT ON TABLE "template"                  IS 'Subject skeleton areas to be used within a template';
-COMMENT ON COLUMN "template"."name"          IS 'Name for template';
-COMMENT ON COLUMN "template"."description"   IS 'Description for template';
-
-CREATE TABLE "skeleton_area" (
-        "id"                    integer         PRIMARY KEY,
-        "name"                  TEXT,
-        "active"                boolean         NOT NULL DEFAULT true,
-        "description"           TEXT );
-COMMENT ON TABLE "skeleton_area"                  IS 'Subject skeleton areas to be used within a template';
-COMMENT ON COLUMN "skeleton_area"."active"        IS 'TRUE means new issues can be created in this area';
-COMMENT ON COLUMN "skeleton_area"."name"          IS 'Name for skeleton area';
-COMMENT ON COLUMN "skeleton_area"."description"   IS 'Description for skeleton area';
-
-CREATE TABLE "skeleton_area_allowed_policy" (
-	 PRIMARY KEY ("skeleton_area_id", "policy_id"),
-        "skeleton_area_id"      INT4            REFERENCES "skeleton_area" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-        "policy_id"             INT4            REFERENCES "policy" ("id") ON DELETE CASCADE ON UPDATE CASCADE, 
-        "default_policy"        BOOLEAN NOT NULL DEFAULT false);
-CREATE UNIQUE INDEX "skeleton_area_allowed_policy_one_default_per_area_idx" ON "skeleton_area_allowed_policy" ("skeleton_area_id") WHERE "default_policy";
-COMMENT ON TABLE "skeleton_area_allowed_policy" IS 'Selects which policies can be used in each skeleton area';
-COMMENT ON COLUMN "skeleton_area_allowed_policy"."default_policy" IS 'One policy per skeleton area can be set as default.';
+COMMENT ON TABLE "template"                  IS 'Template for areas';
+COMMENT ON COLUMN "template"."name"          IS 'Name for the template';
+COMMENT ON COLUMN "template"."description"   IS 'Description for the template';
 
 CREATE TABLE "template_area" (
-        PRIMARY KEY ("template_id", "skeleton_area_id"),
-        "template_id"           INT4            REFERENCES "template" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-        "skeleton_area_id"      INT4            REFERENCES "skeleton_area" ("id") ON DELETE CASCADE ON UPDATE CASCADE );
-COMMENT ON TABLE "template_area"             IS 'Selects skeleton areas belonging to a template';
+        "id"                    INTEGER         PRIMARY KEY,
+        "template_id"           INTEGER REFERENCES "template" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+        "name"                  TEXT,
+        "active"                BOOLEAN         NOT NULL DEFAULT true,
+        "description"           TEXT );
+COMMENT ON TABLE "template_area"                  IS 'Skeleton areas to be used within a template';
+COMMENT ON COLUMN "template_area"."active"        IS 'TRUE means new issues can be created in this area';
+COMMENT ON COLUMN "template_area"."name"          IS 'Name for skeleton area';
+COMMENT ON COLUMN "template_area"."description"   IS 'Description for skeleton area';
+
+CREATE TABLE "template_area_allowed_policy" (
+        PRIMARY KEY ("template_area_id", "policy_id"),
+        "template_area_id"      INT4         REFERENCES "template_area" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+        "policy_id"             INT4         REFERENCES "policy" ("id") ON DELETE CASCADE ON UPDATE CASCADE, 
+        "default_policy"        BOOLEAN NOT NULL DEFAULT false);
+CREATE UNIQUE INDEX "template_area_allowed_policy_one_default_per_area_idx" ON "template_area_allowed_policy" ("template_area_id") WHERE "default_policy";
+COMMENT ON TABLE "template_area_allowed_policy" IS 'Selects which policies can be used in each skeleton area';
+COMMENT ON COLUMN "template_area_allowed_policy"."default_policy" IS 'One policy per skeleton area can be set as default.';
