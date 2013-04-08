44e8659751739c96375f4ba86469271921b91e53
ralzakark <alessandro.caputo@gmail.com>
2013-04-08 15:41:30 +0200

    modified:   core.sql new file:   update/core-update.v2.2.1-m5s2.2.1-0.5.sql

--- core.sql
+++ core.sql
@@ -422,6 +422,18 @@ COMMENT ON COLUMN "unit"."parent_id"    IS 'Parent id of tree node; Multiple roo
 COMMENT ON COLUMN "unit"."active"       IS 'TRUE means new issues can be created in areas of this unit';
 COMMENT ON COLUMN "unit"."member_count" IS 'Count of members as determined by column "voting_right" in table "privilege"';
 
+CREATE TABLE "member_login" (
+        PRIMARY KEY ("member_id", "login_time"),
+	"member_id" 		INT4 NOT NULL REFERENCES "member" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+	"login_time" 		TIMESTAMP WITH TIME ZONE NOT NULL,
+	"geolat" 		NUMERIC(10,8), -- Latitude
+	"geolng" 		NUMERIC(11,8), -- Longitude
+	"browser" 		TEXT, -- Browser used
+	"os" 			TEXT); -- Operating system used
+COMMENT ON COLUMN "member_login"."geolat" IS 'Latitude';
+COMMENT ON COLUMN "member_login"."geolng" IS 'Longitude';
+COMMENT ON COLUMN "member_login"."browser" IS 'Browser';
+COMMENT ON COLUMN "member_login"."os" IS 'Operating system';
 
 CREATE TABLE "unit_setting" (
         PRIMARY KEY ("member_id", "key", "unit_id"),
