7240222e84359961e13253429d511edd73248e6e
ralzakark <root@debian>
2013-07-18 15:50:26 +0200

    Tabella per notifica eventi

--- core.sql
+++ core.sql
@@ -1265,6 +1265,15 @@ COMMENT ON COLUMN "session"."additional_secret" IS 'Additional field to store a 
 COMMENT ON COLUMN "session"."member_id"         IS 'Reference to member, who is logged in';
 COMMENT ON COLUMN "session"."lang"              IS 'Language code of the selected language';
 
+CREATE TABLE checked_event (
+        "event_id"               INT4 NOT NULL,
+        "member_id"              INT4 NOT NULL,
+        CONSTRAINT "checked_event_pkey" PRIMARY KEY ("event_id", "member_id"),
+        CONSTRAINT "checked_event_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "event" ("id") MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT "checked_event_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "member" ("id") MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE);
+
+COMMENT ON TABLE "checked_event" IS 'Possibility to filter events';
+
 
 
 ----------------------------------------------
